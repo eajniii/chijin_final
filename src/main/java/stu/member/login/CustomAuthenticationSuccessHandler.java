554fdebb9f5e4f2b92441e8885e67f0615678b91
jisu <jisoo449@naver.com>
2024-02-22 16:13:34 +0900

    #32 spring security 설정파일 수정 및 이벤트 핸들러 클래스 추가

---
+++ src/main/java/stu/member/login/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,35 @@
+package stu.member.login;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler{
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+		// 로그인 성공 시 수행할 로직
+	    // 예: 세션에 사용자 정보 저장
+		logger.info("**********************************checkin");
+	    HttpSession session = request.getSession();
+	    User user = (User) authentication.getPrincipal();
+	    session.setAttribute("userInfo", user);
+	    logger.info("**********************************username: "+user.getUsername());
+	    logger.info("**********************************password: "+user.getPassword());
+	    
+	    // 리다이렉션
+	    setDefaultTargetUrl("redirect:/main.do");
+	    super.onAuthenticationSuccess(request, response, authentication);
+	}
+
+}
